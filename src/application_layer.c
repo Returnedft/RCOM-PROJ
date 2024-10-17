7b20a381d4759d35e82d492aac8ee33db9371436
Pedro Borges <puedrogamerpt@gmail.com>
2024-10-17 20:06:10 +0100

    Fixed alarm bugs

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,11 +1,34 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
+#include <string.h>  // for strncpy
 
 #include <stdio.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    LinkLayer linklayer;
+
+    // Copy serialPort string into the struct safely
+    strncpy(linklayer.serialPort, serialPort, sizeof(linklayer.serialPort) - 1);
+
+    // Map role string ("tx" or "rx") to enum
+    if (strcmp(role, "tx") == 0) {
+        linklayer.role = LlTx;
+    } else if (strcmp(role, "rx") == 0) {
+        linklayer.role = LlRx;
+    } else {
+        printf("Error: Invalid role '%s'. Use 'tx' for transmitter or 'rx' for receiver.\n", role);
+        return;
+    }
+
+    // Assign other parameters
+    linklayer.baudRate = baudRate;
+    linklayer.nRetransmissions = nTries;
+    linklayer.timeout = timeout;
+
+    // Call llopen with the properly initialized linklayer
+    llopen(linklayer);
 }
--- src/link_layer.c
+++ src/link_layer.c
@@ -17,25 +17,30 @@
 ////////////////////////////////////////////////
 int llsendSet(const unsigned char *buf, int bufSize){
 
+    signal(SIGALRM, alarmHandler);
+    alarmEnabled = 0;
+
     unsigned char byte = 0;
     int check = 0;
 
-    while (alarmCount < 4){
+    while (alarmCount < 3){
+
         if (alarmEnabled == FALSE){
-          alarm(3);
-          alarmEnabled = TRUE;
-          writeBytesSerialPort(buf, bufSize);
+        
+          alarm(4);
+          if (writeBytesSerialPort(buf, bufSize) == -1) return 1;
           sleep(1);
+          alarmEnabled = TRUE;
         }
 
         int read = readByteSerialPort(&byte);
 
         if ( read == -1 ) return 1;
         else if (read == 0) continue;
-        if (uaState(byte, check)) break;
+        if (uaState(byte, &check)) break;
     }
 
-    printf("Ua received \n");
+    if (check == 5 ) printf("Ua received \n");
     sleep(1);
 
     return 0;
@@ -55,7 +60,7 @@ int llsendUA(){
 
         if ( read == -1 ) return 1;
         else if (read == 0) continue;
-        if (setState(byte, check)) STOP = TRUE;
+        if (setState(byte, &check)) STOP = TRUE;
 
     }
     const unsigned char buf[5] = {FLAG,A2,C2,A2^C2,FLAG};
@@ -70,120 +75,119 @@ int llsendUA(){
 ////////////////////////////////////////////////
 // UASTATE
 ////////////////////////////////////////////////
-int uaState(int byte, int check){
-    switch(check){
+int uaState(int byte, int *check){
+    switch(*check){
                case 0:
                    if (byte==FLAG){
-                       check=1;
+                       *check=1;
                    }
                    else{
-                       check=0;
+                       *check=0;
                    }
                    break;
                case 1:
                    if (byte==A2){
-                       check=2;
+                       *check=2;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                       *check=1;
                    }
                    else{
-                       check=0;
+                       *check=0;
                    }
                    break;
                case 2:
                    if (byte==C2){
-                       check=3;
+                       *check=3;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                       *check=1;
                    }
                    else{
-                       check=0;
+                       *check=0;
                    }
                    break;
                case 3:
                    if (byte == (A2^C2)){
-                       check=4;
+                       *check=4;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                       *check=1;
                    }
                    else{
-                       check=0;
+                       *check=0;
                    }
                    break;
                case 4:
                    if (byte==FLAG){
-                       check=5;
+                       *check=5;
                    }
                    else{
-                       check=0;
+                       *check=0;
                    }
                    break;
            }    
 
-    return (check == 5) ? 1 : 0;
+    return (*check == 5) ? 1 : 0;
 }
 
 ////////////////////////////////////////////////
 // SETSTATE
 ////////////////////////////////////////////////
-
-int setState(int byte, int check){
-    switch(check){
+int setState(int byte, int *check){
+    switch(*check){
                case 0:
                    if (byte==FLAG){
-                       check=1;
+                       *check=1;
                    }
                    else{
-                       check=0;
+                    *check=0;
                    }
                    break;
                case 1:
                    if (byte==A1){
-                       check=2;
+                    *check=2;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                    *check=1;
                    }
                    else{
-                       check=0;
+                    *check=0;
                    }
                    break;
                case 2:
                    if (byte==C1){
-                       check=3;
+                    *check=3;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                    *check=1;
                    }
                    else{
-                       check=0;
+                    *check=0;
                    }
                    break;
                case 3:
                    if (byte == (A1^C1)){
-                       check=4;
+                    *check=4;
                    }
                    else if(byte==FLAG){
-                       check=1;
+                    *check=1;
                    }
                    else{
-                       check=0;
+                    *check=0;
                    }
                    break;
                case 4:
                    if (byte==FLAG){
-                       check=5;
+                    *check=5;
                    }
                    else{
-                       check=0;
+                    *check=0;
                    }
                    break;
            }    
 
-    return (check == 5) ? 1 : 0;
+    return (*check == 5) ? 1 : 0;
 }
 
 
@@ -199,8 +203,12 @@ int llopen(LinkLayer connectionParameters)
     }
     const unsigned char buf [5] = {FLAG,A1,C1, A1 ^ C1, FLAG};
 
-    llsendSet(buf,sizeof(buf));
-    llsendUA();
+    if (connectionParameters.role == LlTx) {
+        llsendSet(buf,sizeof(buf));
+    }
+    else if (llsendUA()) {
+        llwrite(connectionParameters.serialPort,sizeof(connectionParameters.serialPort));
+    }
 
     return 1;
 }
@@ -236,3 +244,269 @@ int llclose(int showStatistics)
     return clstat;
 }
 
+////////////////////////////////////////////////
+// RECEIVEDATA
+////////////////////////////////////////////////
+int receiveData(unsigned char byte, int*check, int n, unsigned char *BCC, unsigned char *last){
+    unsigned char infoFrame;
+    if (n==0){
+        infoFrame = 0x00;
+    }
+    else{
+        infoFrame = 0x80;
+    }
+    switch(*check){
+        case 0:
+            if (byte==FLAG){
+                printf("1");
+            *check=1;
+            }
+            else{
+            *check=0;
+            }
+            break;
+        case 1:
+            if (byte==A1){
+                printf("2");
+            *check=2;
+            }
+            else if(byte==FLAG){
+            *check=1;
+            }
+            else{
+            *check=0;
+            }
+            break;
+        case 2:
+            if (byte==infoFrame){
+            *check=3;
+                printf("3");
+            }
+            else if(byte==FLAG){
+            *check=1;
+            }
+            else{
+            *check=0;
+            }
+            break;
+        case 3:
+            if (byte==(A1 ^ infoFrame)){
+            *check=4;
+                printf("4");
+            }
+            else if(byte==FLAG){
+            *check=1;
+            }
+            else{
+            *check=0;
+            }
+            break;
+        case 4:
+            printf("data");
+            if(byte==FLAG){
+                if (*BCC==*last){
+                *check=5;
+                }else{
+                *check=0;
+                }
+            }
+            else{
+                *BCC = *BCC ^ *last;
+                *last = byte;
+            }
+            break;
+    }
+    return (*check == 5) ? 1 : 0;
+}
+
+////////////////////////////////////////////////
+// RESPONSESTATE
+////////////////////////////////////////////////
+int responseState(int byte, int*check, int n, int *error){
+    unsigned char RR, REJ;
+    if (n==0){
+        RR = RR0;
+        REJ = REJ0;
+    }else{
+        RR = RR1;
+        REJ = REJ1;
+    }
+    switch(*check){
+            case 0:
+                if (byte==FLAG){
+                *check=1;
+                }
+                else{
+                *check=0;
+                }
+                break;
+            case 1:
+                if (byte==A2){
+                *check=2;
+                }
+                else if(byte==FLAG){
+                *check=1;
+                }
+                else{
+                *check=0;
+                }
+                break;
+            case 2:
+                if (byte==RR){
+                *check=3;
+                    *error=0;
+                }
+                else if (byte==REJ){
+                *check=3;
+                    *error=1;
+                }
+                else if(byte==FLAG){
+                *check=1;
+                }
+                else{
+                *check=0;
+                }
+                break;
+            case 3:
+                if (*error == 0 || byte == (A2^RR)){
+                *check=4;
+                }
+                else if (*error == 1 || byte == (A2^REJ)){
+                *check=4;
+                }
+                else if(byte==FLAG){
+                *check=1;
+                }
+                else{
+                *check=0;
+                }
+                break;
+            case 4:
+                if (byte==FLAG){
+                *check=5;
+                }
+                else{
+                *check=0;
+                }
+                break;
+        }
+    return (*check == 5) ? 1 : 0;
+}
+
+////////////////////////////////////////////////
+// TRANSMITERDISCSTATE
+////////////////////////////////////////////////
+int transmiterDiscState(int byte, int*check){
+    switch( *check){
+               case 0:
+                   if (byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 1:
+                   if (byte==A2){
+                    *check=2;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 2:
+                   if (byte==DISC){
+                    *check=3;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 3:
+                   if (byte == (A2^DISC)){
+                    *check=4;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 4:
+                   if (byte==FLAG){
+                    *check=5;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+           }    
+
+    return (*check == 5) ? 1 : 0;
+}
+
+////////////////////////////////////////////////
+// RECEIVERDISCSTATE
+////////////////////////////////////////////////
+int receiverDiscState(int byte, int*check){
+    switch(*check){
+               case 0:
+                   if (byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 1:
+                   if (byte==A1){
+                    *check=2;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 2:
+                   if (byte==DISC){
+                    *check=3;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 3:
+                   if (byte == (A1^DISC)){
+                    *check=4;
+                   }
+                   else if(byte==FLAG){
+                    *check=1;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+               case 4:
+                   if (byte==FLAG){
+                    *check=5;
+                   }
+                   else{
+                    *check=0;
+                   }
+                   break;
+           }    
+
+    return (*check == 5) ? 1 : 0;
+}
